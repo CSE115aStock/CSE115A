e271be048cb4ada15f0f887b11f046503741c157
唐秋霞 <tangqiuxia@kresge-100-64-81-21.resnet.ucsc.edu>
2022-01-20 19:09:22 -0800

    account creation page

---
+++ account_creation_page.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+with st.form("account_creation_form"):
+
+    st.header("Social Stock Analyzer")
+    st.subheader("Create your Account")
+
+    Username = st.text_input('',placeholder='Username')
+    Password = st.text_input('',placeholder='Password')
+    Confirm_password = st.text_input('',placeholder='Confirm password')
+    Email = st.text_input('',placeholder='E-mail address')
+
+    submitted = st.form_submit_button("Sign Up")
+    if submitted:
+        username = Username
+        password = Password
+        email = Email
+
+    st.write("Have an account? Sign In")
+
